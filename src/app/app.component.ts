4143735c2ed8617ebbe65dff041939d7f4182395
Mario Cruz <cruzperezmario802@gmail.com>
2023-09-20 18:42:15 -0600

    20/09/2023 MECP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl:'./app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angularEjemplo';
 
-  apellido = 'Cruz';
+/*   apellido = 'Cruz';
 
   duplicarNumero(valor:number):number{
     return valor*2;
   }
+
   pelicula={
     titulo:'Spiderman',
     fechaLanzamiento:new Date(),
     precio: 75.56
-  }
+  } */
+  //<app-product-list></app-product-list>
 }
