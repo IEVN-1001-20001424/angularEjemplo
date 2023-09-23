a0439da21700caef45ae753a35be7386ddc9676b
Mario Cruz <cruzperezmario802@gmail.com>
2023-09-22 20:42:14 -0600

    22/09/23 MECP

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../iproductos';
 
 @Component({
   selector: 'app-product-list',
@@ -6,36 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  productos:any[]=[
+//directivas *ngif
+
+  imageWidth:number=50;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImg=!this.muestraImg;
+  }
+
+  productos:IProductos[]=[
     {
       "productoId":1,
       "Modelo":"Sentra",
       "Descripcion":"4 puertas familiar",
-      "Year":"Febrero 3 2020",
+      "year":"Febrero 3 2020",
       "Precio":120000,
       "Marca":"NISSAN",
       "Color":"Morado",
-      "imagenUrl":"datos pendientes"
+      "imagenUrl":"https://imagenes.20minutos.es/files/article_default_content/uploads/imagenes/2023/04/10/coche-xpeng-g3i.png",
     },
     {
       "productoId":2,
       "Modelo":"A4",
       "Descripcion":"4 puertas familiar",
-      "Year":"Febrero 3 2021",
+      "year":"Febrero 3 2021",
       "Precio":200000,
       "Marca":"AUDI",
       "Color":"Blanco",
-      "imagenUrl":"datos pendientes"
+      "imagenUrl":"https://cdn.imagin.studio/getImage?&customer=carwow&tailoring=carwow&make=kia&modelFamily=sportage&modelRange=sportage&modelVariant=od&modelYear=2022&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0014&fileType=png&width=1200&angle=23&billingTag=web",
     },
     {
       "productoId":3,
       "Modelo":"Rio",
       "Descripcion":"2 puertas familiar",
-      "Year":"Marzo 5 2022",
+      "year":"Marzo 5 2022",
       "Precio":150000,
       "Marca":"KIA",
       "Color":"Azul",
-      "imagenUrl":"datos pendientes"
-    }
+      "imagenUrl":"https://www.lavanguardia.com/files/content_image_mobile_filter/uploads/2022/04/29/626b93732a18a.jpeg",
+    },
   ]
+
 }
