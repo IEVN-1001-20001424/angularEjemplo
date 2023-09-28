84492490b1205bacf71d60cb0a44e02592568bd7
Mario Cruz <cruzperezmario802@gmail.com>
2023-09-27 18:45:50 -0600

    27/09/23 MECP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { ProductListComponent } from './product/product-list/product-list.compon
 import { FormsModule } from '@angular/forms';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
+import { CalculosComponent } from './formularios/calculos/calculos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductoFilterPipe,
-    OperasBasComponent
+    OperasBasComponent,
+    CalculosComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
